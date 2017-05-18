1a44f24246b9e8294d4ef5896cbd6ac7a2300af0
David Schrock <tubust@hotmail.com>
2017-05-18 14:08:31 -0700

    Added Report Views, Quick Check In, Entity Framework

--- SCCheckinV3/Controllers/QuickCheckInController.cs
+++ SCCheckinV3/Controllers/QuickCheckInController.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using SCCheckinV3;
 
 namespace SCCheckinV3.Controllers
 {
     public class QuickCheckInController : Controller
     {
+        private SCCheckInEntities db = new SCCheckInEntities();
+
         // GET: QuickCheckIn/Create
         public ActionResult Create()
         {
@@ -15,19 +21,29 @@ namespace SCCheckinV3.Controllers
         }
 
         // POST: QuickCheckIn/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CheckInID,MemberID,FullName,FirstName,LastName,PaidType,PaidAmount,PaidDate,PaidDesc,DanceType,CreateDate,ReferredBy")] CheckIn checkIn)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
+                db.CheckIns.Add(checkIn);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-                return RedirectToAction("Create");
-            }
-            catch
+            return View(checkIn);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                return View();
+                db.Dispose();
             }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ SCCheckinV3/Controllers/ReportController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SCCheckinV3;
+
+namespace SCCheckinV3.Controllers
+{
+    public class ReportController : Controller
+    {
+        private SCCheckInEntities db = new SCCheckInEntities();
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Report/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Report/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Report/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Report/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Report/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
