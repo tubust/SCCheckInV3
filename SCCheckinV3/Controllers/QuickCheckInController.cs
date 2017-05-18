62a6a2c7b253366c7e36cb5f7f3509ebaaf324e0
Dave Schrock <Dave Schrock>
2017-05-17 18:11:59 -0700

    Add project files.

---
+++ SCCheckinV3/Controllers/QuickCheckInController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCCheckinV3.Controllers
+{
+    public class QuickCheckInController : Controller
+    {
+        // GET: QuickCheckIn/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QuickCheckIn/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Create");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SCCheckinV3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SCCheckinV3.Startup))]
+namespace SCCheckinV3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
