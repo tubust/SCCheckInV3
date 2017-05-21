b5bd5850d4ad73d2c8b424de8042df58f1fad934
David Schrock <tubust@hotmail.com>
2017-05-21 08:17:58 -0700

    Added Remaining Basic Report Functions to the Reports Controller.

--- SCCheckinV3/Controllers/ReportController.cs
+++ SCCheckinV3/Controllers/ReportController.cs
@@ -46,6 +46,125 @@ namespace SCCheckinV3.Controllers
             return View();
         }
 
+        public ActionResult CurrentlyPaidMembers()
+        {
+            return View();
+        }
+
+        public ActionResult DancersCurrentlyInLessons()
+        {
+            return View();
+        }
+
+        public ActionResult DeletedMembers()
+        {
+            var deletedMemberList = db.DeletedMembers.ToList();
+            ViewBag.DeletedMemberList = deletedMemberList;
+            return View();
+        }
+
+        public ActionResult EmailList()
+        {
+            var theEmailList = db.OKSwingMemberLists.Where(em => em.EmailAddress != null && em.EmailAddress != string.Empty);
+            ViewBag.EmailList = theEmailList;
+            return View();
+        }
+
+        public ActionResult ExpiringMembers()
+        {
+            return View();
+        }
+
+        public ActionResult GreenDancers()
+        {
+            return View();
+        }
+
+        public ActionResult MembersModifiedInDatabase()
+        {
+            return View();
+        }
+
+        public ActionResult MissingInAction()
+        {
+            return View();
+        }
+
+        public ActionResult MonthlyDancers()
+        {
+            return View();
+        }
+
+        public ActionResult NewDancers()
+        {
+            return View();
+        }
+
+        public ActionResult NewMembers()
+        {
+            return View();
+        }
+
+        public ActionResult NonReturningMembers()
+        {
+            return View();
+        }
+
+        public ActionResult PinkDancers()
+        {
+            return View();
+        }
+
+        public ActionResult PurpleDancers()
+        {
+            return View();
+        }
+
+        public ActionResult RenewingMembers()
+        {
+            return View();
+        }
+
+        public ActionResult SpecialEvents()
+        {
+            return View();
+        }
+
+        public ActionResult Teachers()
+        {
+            return View();
+        }
+
+        public ActionResult TodaysDancers()
+        {
+            return View();
+        }
+
+        public ActionResult TodaysPayingDancers()
+        {
+            return View();
+        }
+
+        public ActionResult TodaysSummary()
+        {
+            return View();
+        }
+
+        public ActionResult UnknownDancers()
+        {
+            return View();
+        }
+
+        public ActionResult VoidedEntries()
+        {
+            return View();
+        }
+
+        public ActionResult YearlyDues()
+        {
+            return View();
+        }
+
         public ActionResult convertToExcel(int whichReport)
         {
             switch (whichReport)
